d659c68f16e9befd608bfd09bcd8f88b6b352bac
Kofeinich <59574446+Kofeinich@users.noreply.github.com>
2020-12-26 09:42:25 +0300

    Update script_2.bat

--- lab6/second/script_2.bat
+++ lab6/second/script_2.bat
@@ -1,2 +1,2 @@
 @echo off
-forfiles /P "C:\WINDOWS" /C "cmd /c if @fsize GEQ 2097152 copy @path \%computername%\temp /Z /Y" 
+forfiles /P "C:\WINDOWS" /C "cmd /c if @fsize GEQ 2097152 copy @path \%computername%\t /Z /Y" 
